e268aad5e641c2b4b314089db20538794c495f6a
David <davidarrozaqi085@gmail.com>
2023-06-16 02:21:34 +0700

    Create Variable

---
+++ Variable.py
@@ -0,0 +1,12 @@
+# Direkomendasikan menggunakan variable untuk menyimpan String
+p = "David Arrozaqi"
+
+print (p)
+print (p)
+print (p)
+
+p = "Hallo Dunia" # Mengubah data String di pertengahan
+
+print (p)
+print (p)
+print (p)
