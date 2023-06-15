61f4a7539d862d1963d261c9cd14caa360a5a1fd
David <davidarrozaqi085@gmail.com>
2023-06-16 02:41:14 +0700

    Create String Format

---
+++ String.py
@@ -0,0 +1,17 @@
+# "" = ''
+
+Nama = "David Arrozaqi"
+Hobby = 'Rebahan'
+
+nama_depan = "David"
+nama_belakang = "Arrozaqi"
+nama_lengkap = nama_depan + " " + nama_belakang # Untuk Menggabung 2 variable
+sapa = "Hii " + nama_depan + " " + nama_belakang # melelahkan
+
+# String Fromat
+sapa2 = f"Hii {nama_depan} {nama_belakang}" # ini baru ez
+# jika f dihapus maka yang didalam "" akan terbaca string semua
+
+print (nama_lengkap)
+print(sapa)
+print(sapa2)
