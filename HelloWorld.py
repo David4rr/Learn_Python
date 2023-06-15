6bf945730ed07e7cd825249aca83f90412815191
David <davidarrozaqi085@gmail.com>
2023-06-16 01:48:12 +0700

    basic 2

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,11 @@
-print("Hello World!")
 # Komentar ketika menggunakan python
+print("Hello World!")
+# untuk menjalankan String menggunakan ""
 print("Selamat Belajar Python")
+# untuk angka langsung saja
+print(22)
+# jika angka menggunakan "" itu termasuk string
+# Perbedaanya string tidak bisa digunakan untuk operasi matematika
+print("22")
+# Jika ingin angka desimal menggunakan .
+print(1.5)
