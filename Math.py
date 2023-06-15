cf252df0eec6ea062b5a31db0f8ad0298dcce932
David <davidarrozaqi085@gmail.com>
2023-06-16 01:58:37 +0700

    Simple Math

---
+++ Math.py
@@ -0,0 +1,12 @@
+# Operasi sederhana 
+
+# Tambah
+print (1 + 1) # Menggunakan +
+# Kurang
+print (10 - 5) # Menggunakan -
+# Bagi
+print (10 / 5) # Menggunakan /
+# Kali
+print (10 * 5) # Menggunakan *
+# Sisa Bagi
+print (10 % 3) # Menggunakan %
