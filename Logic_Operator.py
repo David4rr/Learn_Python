ef00a00f30d6643e0fc72afa034a5514d58ba3d0
David <davidarrozaqi085@gmail.com>
2023-06-16 15:44:05 +0700

    Create LogicOperator

---
+++ Logic_Operator.py
@@ -0,0 +1,15 @@
+# operator Logika
+
+# Dalam Operator Logika Cuma ada dua and dengan or
+
+print (True and True)
+print (True and False)
+print (False and True)
+print (False and False)
+
+print (True or True)
+print (True or False)
+print (False or True)
+print (False or False)
+
+# masing masing memiliki 4 kemungkinan
