eaadabcad038c0ebe39cc39cba1d7780737fd0c7
David <davidarrozaqi085@gmail.com>
2023-06-16 15:20:20 +0700

    Create InputData

---
+++ InputData.py
@@ -0,0 +1,17 @@
+# Input Data
+
+print ("Silahkan Masukan Nama Anda Disini : ")
+nama = input() # Input ini bersifat String
+
+print(f"Hello {nama}, Selamat Datang")
+
+# Input Integer
+
+print ("Angka Pertama : ")
+a = int ( input() ) # pastikan inputan adalah angka
+
+print ("Angka Kedua : ")
+b = int ( input() )
+
+hasil = a + b
+print (f" {a} + {b} = {hasil} ")
