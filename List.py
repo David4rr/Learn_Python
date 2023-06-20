a9e7a604ce463fda84e79fbd47c5e54691aabdc8
David <davidarrozaqi085@gmail.com>
2023-06-20 23:03:22 +0700

    Create List

---
+++ List.py
@@ -0,0 +1,13 @@
+# List menggunakan []
+
+nama = ["Akbar", "Akber", "Akber esten"]
+nama.append("Akber Winet") # untuk menambahkan data dalam list
+
+print (nama[0]) # Untuk memanggil index dan dimulai dari 0
+print (len(nama)) # Untuk mengetahui jumlah data dalam list
+
+nama.remove("Akbar") # Mengahpus data list
+print (nama)
+print (len(nama))
+
+#! Saat Menghapus data perhatikan posisi indeks bisa saja berubah
