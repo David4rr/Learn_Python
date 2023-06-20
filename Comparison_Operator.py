9e84812ff3fad8861ef2a36f2584715536884dcd
David <davidarrozaqi085@gmail.com>
2023-06-20 15:20:08 +0700

    Create Comparison_Operator

---
+++ Comparison_Operator.py
@@ -0,0 +1,14 @@
+# Operator perbandingan
+# Nilai yang dikeluarkan adalah Boolean
+
+print (1 < 2) # Kurang dari
+print (1 > 2) # Lebih dar
+print (1 <= 2) # Kurang dari sama dengan
+print (1 >= 2) # Lebih dari sama dengan
+print (1 == 2) # Sama dengan
+print (1 != 2) # Tidak sama dengan
+
+# Tidak sama dengan dan sama dengan bisa digunakan untuk String
+
+print ("David" == "David")
+print ("David" != "David")
