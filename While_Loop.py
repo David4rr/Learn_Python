5cbba17676b11d6362eb7d52e4c3ca216ccca53f
David <davidarrozaqi085@gmail.com>
2023-06-20 23:51:32 +0700

    Create While Loop

---
+++ While_Loop.py
@@ -0,0 +1,7 @@
+# perulangan menggunakan kondisi
+
+data = ""
+
+while data != "David": # Akan terus Berulang sampai data bernilai false
+    print ("Coba Lagi")
+    data = input ("Masukan Data dengan benar :")
