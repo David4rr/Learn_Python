d01689793561ffac3182fb3cab9d20f8dcfb9ea0
David <davidarrozaqi085@gmail.com>
2023-06-20 15:33:49 +0700

    Create if_Statment

---
+++ If_Statment.py
@@ -0,0 +1,33 @@
+# If-Statment
+# Pengkondisian
+# Tab dalam Python sangat berpengaruh terhadap jalanya Code
+
+# Contoh 1
+manusia = 100
+zombie = 100
+
+if manusia < zombie:
+    print ("Manusia Punah")
+
+if manusia > zombie:
+    print ("Manusia Selamat")
+
+if manusia == zombie:
+    print ("Perang Berkepanjangan")
+
+# Contoh 2
+menang = True
+
+if menang == True:
+    print ("Selamat anda menang")
+
+if menang == False:
+    print ("Silahkan coba lagi")
+
+# Output yang dihasilkan adalah Output yang bernilai true
+
+
+# if kondisi:
+#    melakukan apa?
+#    melakukan hal lain
+# dst # Jika tidak menggunakan tab maka baris code tersebut tidak akan di jalankan
