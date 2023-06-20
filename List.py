bf7b92f12e72209cc1748713a8c56e891c67ce62
David <davidarrozaqi085@gmail.com>
2023-06-20 23:24:11 +0700

    Create Range

--- List.py
+++ List.py
@@ -17,4 +17,15 @@ print (nama)
 for nama_pelanggan in nama: # Semua data nama akan dimasukan ke variable nama_pelanggan
     print (nama_pelanggan)
 
+# Range
+nomor = range(1, 11) # Untuk menampilkan batas 10 maka harus dilebihkan 1 yaitu 11
+# range digunakan untuk mempermudah penginputan list
+
+for nomor_urut in nomor:
+    print (nomor_urut)
+
+# Range versi 2
+for no in range(1, 11):
+    print (no)
+
 #! Saat Menghapus data perhatikan posisi indeks bisa saja berubah
