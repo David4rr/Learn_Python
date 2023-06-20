24e9759b655249c35591b87c63627bf0115f4441
David <davidarrozaqi085@gmail.com>
2023-06-20 23:07:34 +0700

    Update List

--- List.py
+++ List.py
@@ -1,7 +1,7 @@
 # List menggunakan []
 
 nama = ["Akbar", "Akber", "Akber esten"]
-nama.append("Akber Winet") # untuk menambahkan data dalam list
+nama.append("Akber Winet") # Menambahkan data list
 
 print (nama[0]) # Untuk memanggil index dan dimulai dari 0
 print (len(nama)) # Untuk mengetahui jumlah data dalam list
@@ -9,5 +9,8 @@ print (len(nama)) # Untuk mengetahui jumlah data dalam list
 nama.remove("Akbar") # Mengahpus data list
 print (nama)
 print (len(nama))
+
+nama[0] = "Akber Wikwok" # Mengubah data list
+print (nama)
 
 #! Saat Menghapus data perhatikan posisi indeks bisa saja berubah
